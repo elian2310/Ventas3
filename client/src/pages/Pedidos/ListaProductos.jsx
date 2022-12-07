e194b60e1eb55da815099799d2ce5173d0668431
Leo2464 <leonardo.fuentes.pereira@gmail.com>
2022-12-06 21:58:07 -0400

    Pedidos

---
+++ client/src/pages/Pedidos/ListaProductos.jsx
@@ -0,0 +1,39 @@
+// Importaciones basicas
+import React from "react";
+
+// Importaciones de Estilos
+import "./styles.css";
+import styled from "styled-components"
+
+// Importaciones
+import { productos_falsos } from "./DatosFalsos";
+import { Producto } from "./Producto";
+
+// Estilos
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+  justify-content: space-around;
+  border: 1px solid;
+`;
+
+const Prods = styled.div`
+  display: flex;
+  padding: 20px;
+  flex: 3;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+// Codigo
+export const ListaProductos = () => {
+  return (
+    <Container>
+      <Prods>
+        {productos_falsos.map((item) => (
+          <Producto item={item} key={item.id} />
+        ))}
+      </Prods>
+    </Container>
+  );
+};
---
+++ client/src/pages/Pedidos/Producto.jsx
@@ -0,0 +1,124 @@
+// Importaciones basicas
+import React from "react";
+
+// Importaciones de Estilos
+import "./styles.css";
+import styled from "styled-components";
+
+// Importaciones
+import { AiFillStar } from "react-icons/ai";
+
+// Estilos (Producto)
+const ContenedorProducto = styled.div`
+  padding: 10px;
+  margin: 20px;
+  border: 5px solid;
+  border-radius: 15px;
+  display: flex;
+  text-align: justify;
+  gap: 30px;
+  height: 25vh;
+  width: 80vh;
+  position: relative;
+  box-shadow: 10px 10px 10px lightgray;
+`;
+
+const ContenedorIzquierda = styled.div`
+  border: 1px solid;
+  border-radius: 15px;
+  width: 25vh;
+`;
+
+const ContenedorCentro = styled.div`
+  border: 1px solid;
+  width: 30vh;
+`;
+
+const ContenedorDerecha = styled.div`
+  border: 1px solid;
+  width: 15vh;
+  text-align: center;
+  align-items: center;
+`;
+
+const Imagen = styled.img`
+  border: 1px solid;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 15px;
+
+`;
+
+const Info = styled.div`
+  border: 1px solid;
+  height: 100%;
+  width: 100%;
+`;
+
+const TituloProducto = styled.h2`
+  font-weight: bold;
+`;
+
+// Estilos (Botones)
+const Boton1 = styled.button`
+  background: #575fc9;
+  color: white;
+  outline: 0px;
+  padding: 5px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Boton2 = styled.button`
+  background: fixed;
+  color: #575fc9;
+  outline: 0px;
+  padding: 5px 10px;
+  border: solid;
+  border-radius: 5px;
+  border-color: #575fc9;
+  cursor: pointer;
+`;
+
+// Funciones boton
+function clickVerDetalles() {
+  alert("viendo detalles");
+}
+
+function clickVerAgregar() {
+  alert("Se agrego su producto!");
+}
+
+// Codigo
+export function Producto({ item }) {
+  return (
+    <ContenedorProducto>
+      <ContenedorIzquierda>
+        <Imagen src="/logo192.png" />
+      </ContenedorIzquierda>
+      <ContenedorCentro>
+        <Info>
+          <TituloProducto>{item.Nombre}</TituloProducto>
+          <div>
+            <AiFillStar style={{ color: "red" }} />
+            <AiFillStar style={{ color: "red" }} />
+            <AiFillStar style={{ color: "red" }} />
+            <AiFillStar style={{ color: "red" }} />
+            <AiFillStar style={{ color: "red" }} />
+          </div>
+          <p>Descripcion:</p>
+          <p></p>
+        </Info>
+      </ContenedorCentro>
+      <ContenedorDerecha>
+        <h2>{item.Costo}</h2>
+        <div>Envio Gratis</div>
+        <div style={{ marginTop: "25px" }}>
+          <Boton1 onClick={clickVerDetalles}>Ver Detalles</Boton1>
+          <Boton2 onClick={clickVerAgregar}>Agregar</Boton2>
+        </div>
+      </ContenedorDerecha>
+    </ContenedorProducto>
+  );
+}
